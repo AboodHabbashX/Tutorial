cb162d037523bedf6db86ca74c2518c02aa129c8
AboodHabbashX <amcbushido@gmail.com>
2023-09-17 18:03:20 +0300

    Tutorial

---
+++ SQLConnection/com/Abood/TestConnection.java
@@ -0,0 +1,37 @@
+package com.Abood;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection(
+              "jdbc:mysql://127.0.0.1:3306/jdbc-test",
+              "your username here",
+              "your password here"
+            );
+
+            PreparedStatement preparedStatement =// use your table name instead people
+                    connection.prepareStatement("select * from people");
+
+            ResultSet rs =preparedStatement.executeQuery();
+
+            while (rs.next()){
+                String name= rs.getString("firstname"); // use your column Label Instead firstname
+                int id = rs.getInt("id"); // use your column Label Instead id
+
+                System.out.println("name: "+name +"\n" +"id: "+id);
+            }
+        }catch(Exception e) {
+
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
